029024557e8551780d6ba6eb75699138416157b6
JanhviSirohi <48641063+JanhviSirohi@users.noreply.github.com>
2023-04-10 01:23:53 +0930

    Update gui.py

--- src/gui.py
+++ src/gui.py
@@ -29,7 +29,7 @@ from time import time as now
 # HANDLERS
 
 def on_api_key_change():
-	openai.api_key = os.getenv('OPENAI_KEY')
+	api_key = ss.get('api_key') or os.getenv('OPENAI_KEY')
 	model.use_key(api_key) # TODO: empty api_key
 	#
 	if 'data_dict' not in ss: ss['data_dict'] = {} # used only with DictStorage
@@ -44,9 +44,9 @@ def on_api_key_change():
 	ss['debug']['storage.class'] = ss['storage'].__class__.__name__
 
 
-#ss['community_user'] = os.getenv('COMMUNITY_USER')
-#if 'user' not in ss and ss['community_user']:
-	#on_api_key_change() # use community key
+ss['community_user'] = os.getenv('COMMUNITY_USER')
+if 'user' not in ss and ss['community_user']:
+	on_api_key_change() # use community key
 
 # COMPONENTS
 
@@ -321,8 +321,7 @@ with st.sidebar:
 		ui_task()
 		ui_hyde_prompt()
 
-api_key = os.getenv('OPENAI_KEY')
-#ui_api_key()
+ui_api_key()
 ui_pdf_file()
 ui_question()
 ui_hyde_answer()
